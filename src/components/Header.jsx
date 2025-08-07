42b2e8803d6ac31e21e45cc653cfcbc056077ef4
Stefan Airoaie <airoaiestefan@gmail.com>
2025-08-07 13:40:04 +0200

    new meniu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,12 +8,13 @@ const navigation = [
     { name: 'Gestionarea emoțiilor', href: '/emotii' },
     { name: 'Sănătate mintală', href: '/sanatate' },
 ]
+import Image from 'next/image';
+import Link from "next/link";
+
 import {
     Dialog,
     DialogPanel,
     Disclosure,
-    DisclosureButton,
-    DisclosurePanel,
     Popover,
     PopoverButton,
     PopoverGroup,
@@ -21,60 +22,102 @@ import {
 } from '@headlessui/react';
 
 
+const links = [
+    { name: "Über uns", href: "/#about" },
+    { name: "Umsetzung", href: "/#umsetzung" },
+    { name: "Referenzen", href: "/#referenzen" },
+    { name: "Team", href: "/#team" },
+    { name: "Produktkataloge", href: "/#produktkataloge" },
+    { name: "FAQ", href: "/#faq" },
+];
+
 const Header = () => {
 
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-
     return (
         <>
             {/* Header */}
-            <header className="absolute inset-x-0 top-0 z-50">
-                <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
-                    <div className="flex lg:flex-1">
-                        <a href="/" className="-m-1.5 p-1.5">
-                            <span className="sr-only">psihorelatii.ro</span>
-                            <img
-                                alt="Logo psihorelatii.ro"
-                                src="/logo.png"
-                                className="h-8 w-auto"
-                            />
-                        </a>
-                    </div>
-                    <div className="flex lg:hidden">
+            <header className="bg-white shadow-lg sticky top-0 py-4 z-20 text-dark">
+                {/* Desktop Nav */}
+                <div className="container mx-auto flex justify-between items-center">
+                    {/* Logo */}
+                    <a href="/" className="justify-center items-center">
+                        <Image
+                            src="/logo.png"
+                            alt="Logo psihorelatii.ro"
+                            width={200}
+                            height={100}
+                        />
+                    </a>
+                    {/* Burger Menu Icon */}
+                    <div className="flex lg:hidden justify-end">
                         <button
                             type="button"
                             onClick={() => setMobileMenuOpen(true)}
                             className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
                         >
                             <span className="sr-only">Open main menu</span>
-                            <Bars3Icon aria-hidden="true" className="size-6" />
+                            <Bars3Icon className="w-10 h-10" aria-hidden="true" />
                         </button>
                     </div>
-                    <div className="hidden lg:flex lg:gap-x-12">
-                        {navigation.map((item) => (
-                            <a key={item.name} href={item.href} className="text-sm/6 font-semibold text-primary">
-                                {item.name}
-                            </a>
-                        ))}
-                    </div>
-                    <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-                        <a href="/#" className="text-sm/6 font-semibold text-primary btn btn-accent">
-                            Afla mai multe
+                    {/* Nav */}
+                    <div className="hidden lg:flex items-center gap-8">
+                        <nav className="flex gap-8">
+                            <PopoverGroup className="hidden gap-3 lg:flex ">
+                                {/* Services */}
+                                <Popover className="relative">
+                                    {/* <PopoverButton className="flex justify-center items-center text-sm font-semibold text-dark  hover:text-accent transition">
+                                        Dienstleistungen
+                                        <ChevronDownIcon className="h-5 w-5 text-accent" aria-hidden="true" />
+                                    </PopoverButton> */}
+
+                                    <PopoverPanel className="absolute left-0 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
+                                        <div className="p-4">
+
+                                        </div>
+                                    </PopoverPanel>
+                                </Popover>
+                                {/* Links */}
+                                {navigation.map((subLink, j) => (
+                                    <Popover key={j} >
+                                        <PopoverButton className="flex justify-center items-center text-sm font-semibold text-dark">
+                                            <Link className="pr-4 hover:text-accent transition"
+                                                href={subLink.href}
+
+                                            >
+                                                {subLink.name}
+                                            </Link>
+
+                                        </PopoverButton>
+                                    </Popover>
+                                ))}
+                            </PopoverGroup>
+                        </nav>
+                        {/* Call to Action */}
+                        <a
+                            className="btn bg-accent text-white px-2 py-2 rounded hover:bg-opacity-90 transition"
+                            href="#anfrage"
+                        >
+                            Call To Action
                         </a>
                     </div>
-                </nav>
+                </div>
+
+                {/* Mobile NAV */}
                 <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
                     <div className="fixed inset-0 z-50" />
-                    <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-primary/10">
+                    <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                         <div className="flex items-center justify-between">
-                            <a href="/" className="-m-1.5 p-1.5">
-                                <span className="sr-only">psihorelatii.ro</span>
+                            {/* Logo Mobile Menu */}
+                            <a href="/" className="justify-center items-center">
                                 <img
+                                    src="/logoweila.png"
                                     alt="Logo psihorelatii.ro"
-                                    src="/logo.png"
-                                    className="h-8 w-auto"
+                                    width={200}
+                                    height={100}
                                 />
                             </a>
+                            {/* Close Button*/}
                             <button
                                 type="button"
                                 onClick={() => setMobileMenuOpen(false)}
@@ -84,9 +127,7 @@ const Header = () => {
                                 <XMarkIcon aria-hidden="true" className="size-6" />
                             </button>
                         </div>
-
-
-                        {/* mobile nav */}
+                        {/* Nav */}
                         <div className="mt-6 flow-root">
                             <div className="-my-6 divide-y divide-gray-500/10">
                                 <div className="space-y-2 py-6">
@@ -94,42 +135,44 @@ const Header = () => {
 
                                     </Disclosure>
                                     {/* Links */}
-                                    {navigation?.map((subLink, j) => (
+                                    {navigation.map((subLink, j) => (
                                         <Popover key={j} >
                                             <PopoverButton className="flex justify-center items-center text-sm font-semibold text-dark">
-                                                <a
+                                                <Link
                                                     className="pr-4 hover:text-accent transition"
                                                     href={subLink.href}
                                                     onClick={() => setMobileMenuOpen(false)}
                                                 >
                                                     {subLink.name}
-                                                </a>
+                                                </Link>
                                             </PopoverButton>
                                         </Popover>
                                     ))}
                                 </div>
                                 <div className="py-6 flex justify-center" >
-                                    {/* call to action */}
+
+                                    {/* Call to Action */}
+
                                     <a
-                                        onClick={() => setMobileMenuOpen(false)}
-                                        href="/#"
-                                        className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-primary hover:bg-gray-50 btn btn-accent"
+                                        className="btn bg-accent text-white px-2 py-2 rounded hover:bg-opacity-90 transition"
+                                        href="#anfrage"
                                     >
-                                        Afla mai multe
+                                        Call to action
                                     </a>
                                 </div>
                             </div>
                         </div>
-
-
-
                     </DialogPanel>
                 </Dialog>
             </header>
-
-
         </>
     );
 };
 
 export default Header;
+
+
+
+
+
+
